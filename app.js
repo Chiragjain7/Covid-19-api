4aede297e4ac1e0aaf12deb905ad285f047d43b2
Chiragjain7 <55436393+Chiragjain7@users.noreply.github.com>
2021-08-13 20:28:55 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app     = express();
+const port    = 3000 || process.env.PORT;
+
+app.use(express.static(__dirname+'/public'));
+app.set('view engine','ejs');
+
+const globalData =  require('./utils/globalData');
+const news       =  require('./utils/news');
+const indianData    = require('./utils/india')
+
+
+app.get('/',(req,res)=>{
+    res.redirect('/home');
+});
+
+app.get('/home',(req,res)=>{
+    
+    globalData.getGlobal_data()
+    .then(data=> res.render('home',{data : data}))
+    .catch(err=>res.render('page404'))
+});
+
+app.get('/india',(req,res)=>{
+    
+    indianData().then(data=>{
+        news()
+        .then(updates=> res.render('india',{data : data,updates:updates}))
+        }).catch(err=>console.log(err));
+});
+    
+app.get('/global',(req,res)=>{
+//     globalData.tabledata().then(data=> res.send(data));
+   globalData.tabledata()
+   .then(table=> {res.render('global',{table:table})})
+   .catch(err=> res.render('page404'))
+});
+
+app.get('*',(req,res)=>{
+    res.render('page404');
+});
+
+app.listen(port,()=>{
+    console.log('server started on port ',port);
+});
